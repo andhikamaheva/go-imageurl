daba55c13aa24fb0f33c0f177514d3c67da86a02
Andhika Maheva Wicaksono <andhikamaheva@gmail.com>
2018-12-13 14:08:25 +0700

    Updated godoc

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 /*
-// About image-url
-// image-url allows you to find type, size and download image from its url (remote image)
+About image-url
+
+image-url allows you to find type, size and download image from its url (remote image)
 
 */
 package imageurl
